e9d473017e66fc263898bf0a1501dcccdc9356b5
Greg Taschuk <gtaschu1@swarthmore.edu>
2013-03-19 14:15:53 -0400

    started working on callbacks controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,8 +2,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def twitter
     auth = request.env["omniauth.auth"]
-    puts auth
-    #puts request.env["omniauth.extra"]
     #cred = request.env["omniauth.extra"]
     #puts cred
     @user = User.find_by_provider_and_uid(auth.provider, auth.uid)
@@ -15,15 +13,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # https://github.com/intridea/omniauth/wiki/Auth-Hash-Schema
     unless @user
       @user = User.create(
-        provider:auth.provider,
-        uid:auth.uid,
-        email:auth.info.email,
-        password:Devise.friendly_token[0,20]
+        provider:  auth.provider,
+        uid:       auth.uid,
+        email:     auth.info.email,
+        password:  Devise.friendly_token[0,20]
       )
     end
 
     sign_in_and_redirect @user, :event => :authentication
     set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
   end
-
 end
---
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe Users::OmniauthCallbacksController do
+  describe "GET twitter" do
+    before :each do
+      @request.env["devise.mapping"] = Devise.mappings[:user] 
+      # Create the auth stub to match user created above
+      @request.env["omniauth.auth"] = OmniAuth::AuthHash.new ({
+        provider: "twitter",
+        uid: "twitter_uid",
+        credentials: {
+        token: "the token", 
+        secret: "the secret"
+      },
+        info: {
+        email: "greg@gregtaschuk.com"
+      }
+      })
+
+    end
+
+    it "find user when user exists" do
+      # Create the user we'll find
+      u = FactoryGirl.create(:user) 
+
+      User.should_receive(:find_by_provider_and_uid).with(
+        "twitter", 
+        "twitter_uid"
+      )
+
+      get :twitter
+
+      @user = User.find_by_uid("twiiter_uid")
+      @user.should_not be_nil
+    end
+
+    it "create user when user doesn't" do
+      User.should_receive(:find_by_provider_and_uid).with(
+        "twitter", 
+        "twitter_uid"
+      )
+
+      User.should_receive(:create).with(
+        provider: "twitter", 
+        uid:      "twitter_uid",
+        email: "greg@gregtaschuk.com",
+        password: anything()
+      )
+
+      get :twitter
+
+      @user = User.find_by_uid("twiiter_uid")
+      @user.should_not be_nil
+    end
+  end  
+end
